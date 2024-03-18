b5625a71afafd774ab46a66cc99efc626e2b0eeb
tablizovinn <tablizovinn@gmail.com>
2024-03-18 08:55:32 +0800

    Add project files.

---
+++ ChatAppDesktop/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ChatAppDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageRTB = new System.Windows.Forms.RichTextBox();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.messageLbl = new System.Windows.Forms.Label();
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.messageTB = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageRTB
+            // 
+            this.messageRTB.Location = new System.Drawing.Point(468, 37);
+            this.messageRTB.Name = "messageRTB";
+            this.messageRTB.Size = new System.Drawing.Size(300, 351);
+            this.messageRTB.TabIndex = 0;
+            this.messageRTB.Text = "";
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLbl.Location = new System.Drawing.Point(54, 72);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(124, 29);
+            this.usernameLbl.TabIndex = 1;
+            this.usernameLbl.Text = "Username";
+            // 
+            // messageLbl
+            // 
+            this.messageLbl.AutoSize = true;
+            this.messageLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLbl.Location = new System.Drawing.Point(54, 124);
+            this.messageLbl.Name = "messageLbl";
+            this.messageLbl.Size = new System.Drawing.Size(112, 29);
+            this.messageLbl.TabIndex = 2;
+            this.messageLbl.Text = "Message";
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(184, 72);
+            this.usernameTB.Multiline = true;
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(162, 29);
+            this.usernameTB.TabIndex = 3;
+            // 
+            // messageTB
+            // 
+            this.messageTB.Location = new System.Drawing.Point(184, 124);
+            this.messageTB.Multiline = true;
+            this.messageTB.Name = "messageTB";
+            this.messageTB.Size = new System.Drawing.Size(162, 77);
+            this.messageTB.TabIndex = 4;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(184, 207);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(84, 38);
+            this.sendBtn.TabIndex = 5;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 451);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.messageTB);
+            this.Controls.Add(this.usernameTB);
+            this.Controls.Add(this.messageLbl);
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.messageRTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox messageRTB;
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.Label messageLbl;
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.TextBox messageTB;
+        private System.Windows.Forms.Button sendBtn;
+    }
+}
+
---
+++ ChatAppDesktop/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatAppDesktop
+{
+    public partial class Form1 : Form
+    {
+        HubConnection hub_connection;
+        public Form1()
+        {
+            InitializeComponent();
+            hub_connection = new HubConnectionBuilder().WithUrl("https://localhost:7296/chathub").Build();
+
+            hub_connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                messageRTB.Invoke((MethodInvoker)delegate
+                {
+                    messageRTB.AppendText($"{user}: {message}\n");
+                });
+            });
+
+            // Subscribe to Closed event
+            hub_connection.Closed += async (error) =>
+            {
+                // Connection is closed
+                // Handle the error if there is one
+                await Task.Delay(0);
+                MessageBox.Show("SignalR connection closed.");
+                if (error != null)
+                {
+                    MessageBox.Show($"Connection closed due to an error: {error.Message}");
+                }
+            };
+
+            hub_connection.StartAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    MessageBox.Show($"SignalR connection failed to start: {task.Exception.Message}");
+                }
+                else
+                {
+                   
+                    // Enable the send button after the connection is established
+                    sendBtn.Enabled = true;
+                }
+            });
+
+        }
+
+        private void  Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            if(hub_connection.State == HubConnectionState.Connected)
+            {
+                string user = usernameTB.Text;
+                string message = messageTB.Text;
+
+                await hub_connection.SendAsync("SendMessage",user, message);
+
+                messageTB.Clear();
+            }
+            else
+            {
+                MessageBox.Show("SignalR connection is not active.");
+            }
+        }
+    }
+}
